6bef2c4556e9e2def8f8d0b26f0b893444c038d0
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-24 00:54:16 +0900

    Add DemoJavaLibraryCollections

---
+++ src/demo/DemoJavaLibraryCollections.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.Set;
+
+public class DemoJavaLibraryCollections {
+
+	public static void main(String[] args) {
+		
+		Set.of(1, 2, 1); // Exception in thread "main" java.lang.IllegalArgumentException: duplicate element: 1
+
+	}
+
+}
