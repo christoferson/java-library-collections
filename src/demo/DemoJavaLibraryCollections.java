8844504ddd5e7c855f3b7dca2e7b00cbc6e06db1
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-24 23:18:18 +0900

    Add demoPeekAndFlatMap

--- src/demo/DemoJavaLibraryCollections.java
+++ src/demo/DemoJavaLibraryCollections.java
@@ -1,13 +1,32 @@
 package demo;
 
+import java.util.Arrays;
 import java.util.Set;
 
 public class DemoJavaLibraryCollections {
 
 	public static void main(String[] args) {
 		
-		Set.of(1, 2, 1); // Exception in thread "main" java.lang.IllegalArgumentException: duplicate element: 1
+		demoSetOfArgumentMustbeUnique();
+		
+		demoPeekAndFlatMap();
+		
+	}
 
+	private static void demoPeekAndFlatMap() {
+		String[][] arr = {{"a", "d"}, {"n", "d"}, {"a", "x"}};
+		Arrays.stream(arr)
+		  .peek(v -> v.equals(new String[] {"a", "d"}))
+		  .flatMap(u -> Arrays.stream(u))
+		  .forEach(System.out::print);
+	}
+
+	private static void demoSetOfArgumentMustbeUnique() {
+		try {
+			Set.of(1, 2, 1); // Exception in thread "main" java.lang.IllegalArgumentException: duplicate element: 1
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
